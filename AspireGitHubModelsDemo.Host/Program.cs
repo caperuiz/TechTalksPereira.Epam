7dd24048fce627f575e9620518b18e253ffbd455
Carlos_Pena <Carlos_Pena@EPCOBOGW1865>
2025-09-09 09:48:34 -0500

    adding replica support

--- AspireGitHubModelsDemo.Host/Program.cs
+++ AspireGitHubModelsDemo.Host/Program.cs
@@ -2,6 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var chat = builder.AddGitHubModel("chat", "openai/gpt-4o-mini");
 builder.AddProject<Projects.AspireGitHubModelsDemo_WebApplication>("webapp")
-    .WithReference(chat);
+    .WithReference(chat).WithReplicas(1);
 
 builder.Build().Run();
